b427928c0e78601039aa4a0125534e50cf39cfe7
schoko19961 <david.wennemaring@gmail.com>
2022-04-28 17:26:32 +0200

    Added history to population

--- population.py
+++ population.py
@@ -8,26 +8,26 @@ class Population(object):
     def __init__(self, population: List[Genome]) -> None:
         self.population = population
         self.best = self.population[0]
-        self.iteration = 0
+        self.history: List[float] = []
     
     def run(self) -> None:
         for iteration in range(Parametres.MAX_ITERATIONS):
             self.population.sort(key=lambda bag: bag.fitness)
-            # Print new bag if better one was found
+            # Print new Genome if better one was found
             if self.best.fitness < self.population[0].fitness:
-                print(self.population[0])
+                self.best = self.population[0]
+                print(self.best)
             
-            self.best = self.population[0]
-            self.iteration = iteration
-
+            self.history.append(self.best.fitness)
             # Print every 100 iterations
-            if iteration % 100 == 0:
+            if (iteration + 1) % 100 == 0:
                 print(self)
 
 
             new_population = [self.selection().create_child(self.selection()) for _ in range(Parametres.POPULATION_SIZE - 1)]
             new_population.append(self.best)
             self.population = new_population
+        print(self)
 
     def selection(self) -> Genome:
         list_len = len(self.population)
@@ -44,7 +44,7 @@ class Population(object):
         return self.population[indIndex]
 
     def __str__(self) -> str:
-        return "**** Iteration {} ****\n{}".format(self.iteration, self.best.fitness)
+        return "**** Iteration {} -> fitness: {} ****\n{}".format(len(self.history), self.best.fitness, self.best)
 
 
     
